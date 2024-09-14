6d6af8a23969cd09dc2dd261ee19f685b6830213
SorelliGamer <162809184+SorelliGamer@users.noreply.github.com>
2024-09-14 11:58:39 -0300

    aulas3/4
    szyjkhfmszfhndhn

---
+++ Aula 3/index.php
@@ -0,0 +1,58 @@
+<?php
+    include('../Conexoes/conexao.php');
+    
+    //QUERY
+    $sql_codigo = "SELECT p.nome, p.marca, quantidade,(p.preco *quantidade) AS valor_venda FROM `vendas` JOIN produtos p ON p.id = vendas.produto_id ORDER BY valor_venda DESC LIMIT 3";
+
+    $sql_query = $mysqli->query($sql_codigo);
+    
+    $infos = $sql_query->fetch_all();
+
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Top vendas</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main class="container-principal">
+    <h1>
+        Top Vendas
+    </h1>
+    <div class="cont">
+        <div>
+            Nome:
+        </div>
+        <div>
+            Marca:
+        </div>
+        <div>
+            Quantidade:
+        </div>
+        <div>
+            Valor:
+        </div>
+    </div>
+        <?php
+            foreach($infos as $linha){
+                $nome = $linha[0];
+                $marca = $linha[1];
+                $quantidade = $linha[2];
+                $valor = $linha[3];
+                echo'<div class="container">';
+                echo '<div>' .$nome. '</div>';
+                echo '<div>' .$marca. '</div>';
+                echo '<div>' .$quantidade. '</div>';
+                echo '<div>' .$valor. '</div>';
+                echo '</div>';
+            }
+        ?>
+    </main>
+</body>
+</html>
---
+++ Aula 4/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main class="container">
+        <h1>Top Vendas</h1>
+        <form action="verificar.php" method="post" class="conteiner-input">
+            <input type="text" name="pesquisa" id="pesquisa">
+            <button type="submit">🔍</button>
+        </form>
+        <table>
+            <tr>
+                <th>Nome</th>
+                <th>Marca</th>
+                <th>Quantidade</th>
+                <th>Valor</th>
+            </tr>
+            <!-- Código PHP -->
+        </table>
+    </main>
+</body>
+</html>
